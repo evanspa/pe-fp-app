2275a02d823983a054c38f8f948fb2c34ca941d4
Paul Evans <evansp2@gmail.com>
2015-06-06 20:37:47 -0400

    updated version in project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-fp-app "0.0.3"
+(defproject pe-fp-app "0.0.4"
   :description "The fuel purchase application REST API endpoint."
   :url "https://github.com/evanspa/pe-fp-app"
   :license {:name "MIT"
